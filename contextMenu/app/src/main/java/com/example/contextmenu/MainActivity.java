6c5466bb3e6e709852e14d66ec99f3b938434c7d
bnn18u166 <nikita.burtelov@mail.ru>
2019-11-23 13:05:27 +0300

    lesson 5

---
+++ contextMenu/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.contextmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView headTv_1;
+    public TextView headTv_2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
